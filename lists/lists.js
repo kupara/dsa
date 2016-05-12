069202be3bbd4e274a4117099acc3a1ca8ac15e5
andela-ekupara <edwin.kupara@andela.com>
2016-05-12 11:33:43 +0300

    Lists: Finish method implementation

--- lists/lists.js
+++ lists/lists.js
@@ -1,25 +1,25 @@
 function List() {
   this.listSize = 0;
   this.pos = 0;
-  // this.length = listSize;
-  // this.clear = clear;
-  // this.insert = insert;
-  // this.toString = toString;
-  // this.getElement = getElement;
+  this.length = length;
+  this.clear = clear;
+  this.insertAt = insertAt;
+  this.toString = toString;
+  this.getElement = getElement;
   this.find = find;
   this.append = append;
   this.remove = remove;
-  // this.front = front;
-  // this.end = end;
-  // this.prev = prev;
-  // this.next = next;
-  // this.currPos = currPos;
-  // this.moveTo =moveTo;
+  this.front = front;
+  this.end = end;
+  this.prev = prev;
+  this.next = next;
+  this.currPos = currPos;
+  this.moveTo =moveTo;
   this.dataStore = [];
 }
 
 function append(element) {
-  this.dataStore[++this.listSize] = element;
+  this.dataStore[this.listSize++] = element;
 }
 
 function find(element) {
@@ -41,10 +41,84 @@ function remove(element) {
   return false;
 }
 
+function length() {
+  return this.listSize;
+}
+
+function toString() {
+  return this.dataStore;
+}
+
+function insertAt(pos, element) {
+  if(pos <= this.listSize) {
+    this.dataStore.splice(pos, 0, element);
+    ++this.listSize;
+    return true;
+  }
+  throw new RangeError('Out of Bounds');
+}
+
+function clear() {
+  delete this.dataStore;
+  this.dataStore = [];
+  this.listSize = this.pos = 0;
+}
+
+function front() {
+  this.pos = 0;
+}
+
+function end() {
+  this.pos = this.listSize - 1;
+}
+
+function prev() {
+  if(this.pos > 0) {
+    --this.pos;
+  }
+  else {
+    throw new RangeError('Position out of range!');
+  }
+}
+
+function next() {
+  if(this.pos < this.listSize - 1) {
+    ++this.pos;
+  }
+  else {
+    throw new RangeError('Position out of range!');
+  }
+}
+
+function currPos() {
+  return this.pos;
+}
+
+function getElement() {
+  return this.dataStore[this.pos]
+}
+
+function moveTo(pos) {
+  if (pos < this.listSize) {
+    this.pos = pos;
+  }
+  else {
+    throw new RangeError('Position out of range!');
+  }
+}
+
 var testList = new List();
 testList.append(3);
 console.log(testList);
 testList.append(35);
 console.log(testList);
-testList.remove()
-console.log(testList);
+testList.remove(3)
+console.log(testList.toString());
+console.log(testList.length());
+testList.insertAt(1, 22);
+console.log('After insertion into list: ', testList.toString());
+testList.moveTo(1);
+testList.prev();
+console.log('element at this pos: ', testList.getElement());
+testList.clear();
+console.log('Cleared list: ', testList.toString());
