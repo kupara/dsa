25b306fba963a99c15d8456bab847f4062d6b10b
andela-ekupara <edwin.kupara@andela.com>
2016-05-13 13:18:12 +0300

    Stack: Method and property declarations

---
+++ stacks/stack.js
@@ -0,0 +1,26 @@
+function Stack() {
+  this.top = 0;
+  this.length = 0;
+  this.pop = pop;
+  this.push = push;
+  this.clear = clear;
+  this.empty = true;
+  this.peek = peek;
+  this.stack = [];
+}
+
+function push() {
+
+}
+
+function pop() {
+
+}
+
+function clear() {
+
+}
+
+function peek() {
+
+}
