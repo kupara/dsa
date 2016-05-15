def86be190c5dfbbdd6eec661588373fac362521
andela-ekupara <edwin.kupara@andela.com>
2016-05-15 21:18:39 +0300

    Stacks: A palindrome implementation

---
+++ stacks/palindrome-stack.js
@@ -0,0 +1,29 @@
+function isPalindrome(word) {
+  var s = new Stack();
+  for (var i = 0; i < word.length; ++i) {
+    s.push(word[i]);
+  }
+  var rword = "";
+  while (s.length() > 0) {
+    rword += s.pop();
+  }
+  if (word == rword) {
+    return true;
+  } else {
+    Using the Stack Class | 55
+    www.it - ebooks.info
+    return false;
+  }
+}
+var word = "hello";
+if (isPalindrome(word)) {
+  print(word + " is a palindrome.");
+} else {
+  print(word + " is not a palindrome.");
+}
+word = "racecar"
+if (isPalindrome(word)) {
+  print(word + " is a palindrome.");
+} else {
+  print(word + " is not a palindrome.");
+}
