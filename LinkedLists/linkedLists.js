e19e01906cd30b5de32e09cfd597f1bd05c0e6fa
andela-ekupara <edwin.kupara@andela.com>
2016-05-17 15:49:04 +0300

    Linked List implementation and examples

---
+++ LinkedLists/linkedLists.js
@@ -0,0 +1,63 @@
+function Node(elem) {
+  this.element = elem;
+  this.next = null
+}
+
+function LinkedList() {
+  this.head = new Node("head");
+  this.find = find;
+  this.insert = insert;
+  this.remove = remove;
+  this.display = display;
+  this.findPrevious = findPrevious;
+  this.remove = remove;
+}
+
+function find(item) {
+  var currNode = this.head;
+  while (currNode.element != item) {
+    currNode = currNode.next;
+  }
+  return currNode;
+}
+
+function insert(newElement, item) {
+  var newNode = new Node(newElement);
+  var current = this.find(item);
+  newNode.next = current.next;
+  current.next = newNode;
+}
+
+function display() {
+  var currNode = this.head;
+  while (!(currNode.next == null)) {
+    console.log(currNode.next.element);
+    currNode = currNode.next;
+  }
+}
+
+
+//Removing Nodes from a Linked List
+
+function findPrevious(item) {
+  var currNode = this.head;
+  while (!(currNode.next == null) && (currNode.next.element != item)) {
+    currNode = currNode.next;
+  }
+  return currNode;
+}
+
+function remove(item) {
+  var prevNode = this.findPrevious(item);
+  if (!(prevNode.next == null)) {
+    prevNode.next = prevNode.next.next;
+  }
+}
+
+var cities = new LinkedList();
+cities.insert("Nairobi", "head");
+cities.insert("Mombasa", "Nairobi");
+cities.insert("Kisumu", "Mombasa");
+cities.display();
+cities.remove("C");
+cities.display();
